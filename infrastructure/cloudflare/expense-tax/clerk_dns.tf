2c63fde57bab49259d9cb1fd33b55126eb61f81e
Toby Tran <toby.tran@archerirm.com>
2026-09-11 11:19:46 -0500

    fix(infra): add Clerk DMARC record

--- infrastructure/cloudflare/expense-tax/clerk_dns.tf
+++ infrastructure/cloudflare/expense-tax/clerk_dns.tf
@@ -33,3 +33,13 @@ resource "cloudflare_dns_record" "clerk" {
   ttl     = 1
   proxied = false
 }
+
+resource "cloudflare_dns_record" "clerk_dmarc" {
+  zone_id = data.cloudflare_zone.expense_tax.id
+  name    = "_dmarc.tobytran.dev"
+  type    = "TXT"
+  content = "v=DMARC1; p=none; adkim=s; aspf=s"
+  ttl     = 3600
+  proxied = false
+  comment = "Clerk production email authentication"
+}
